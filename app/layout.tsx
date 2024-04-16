77757ac07e2996ebb6be9f37e3eb1b9a84a6b535
Vdolita <vdolita@outlook.com>
2024-04-16 11:44:01 +0800

    cloudflare

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const runtime = "edge";
+
 export const metadata: Metadata = {
   title: "Vdolita",
   keywords: "vdolita",
