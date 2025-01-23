b3c816bc015a200934d2c36aed7ab2b22bae15d8
Vdolita <vdolita@outlook.com>
2025-01-23 10:47:42 +0800

    watch revolver redirect to wrist stability

--- app/watch-revolver/support/page.tsx
+++ app/watch-revolver/support/page.tsx
@@ -1,25 +1,19 @@
+"use client";
+
+import Head from "next/head";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Support() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/wrist-stability/support");
+  }, [router]);
+
   return (
-    <main className="h-screen flex items-center justify-center text-center bg-gray-100">
-      <div>
-        <h1 className="text-3xl font-bold mb-4">Support for Watch Revolver</h1>
-        <p className="text-lg mb-4">
-          Thank you for using <strong>Watch Revolver</strong>. If you have any
-          questions or need assistance, please feel free to contact our support
-          team at:
-        </p>
-        <p className="text-xl font-semibold">
-          <a
-            href="mailto:vdolita.help@outlook.com"
-            className="text-blue-600 underline"
-          >
-            vdolita.help@outlook.com
-          </a>
-        </p>
-        <p className="text-sm mt-4 text-gray-600">
-          We are here to help with any issues related to the Watch Revolver app.
-        </p>
-      </div>
-    </main>
+    <Head>
+      <meta http-equiv="refresh" content="0;url=/wrist-stability/support" />
+    </Head>
   );
 }
---
+++ app/wrist-stability/support/page.tsx
@@ -0,0 +1,25 @@
+export default function Support() {
+  return (
+    <main className="h-screen flex items-center justify-center text-center bg-gray-100">
+      <div>
+        <h1 className="text-3xl font-bold mb-4">Support for Wrist Stability</h1>
+        <p className="text-lg mb-4">
+          Thank you for using <strong>Wrist Stability</strong>. If you have any
+          questions or need assistance, please feel free to contact our support
+          team at:
+        </p>
+        <p className="text-xl font-semibold">
+          <a
+            href="mailto:vdolita.help@outlook.com"
+            className="text-blue-600 underline"
+          >
+            vdolita.help@outlook.com
+          </a>
+        </p>
+        <p className="text-sm mt-4 text-gray-600">
+          We are here to help with any issues related to the Wrist Stability app.
+        </p>
+      </div>
+    </main>
+  );
+}
